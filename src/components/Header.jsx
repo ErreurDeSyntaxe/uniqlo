8450cebce36b27b10c1adf93826c9e1220b7b5f4
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-10-24 15:34:29 +0800

    Add nav to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import styles from './Header.module.css';
+import NavBar from './NavBar';
 
 function Header() {
-  return <h1 className={styles.header}>Uniqlo — The Unique Brand</h1>;
+  return (
+    <>
+      <NavBar />
+      <h1 className={styles.header}>Uniqlo — The Unique Brand</h1>
+    </>
+  );
 }
 
 export default Header;
