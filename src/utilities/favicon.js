e16017817aa7b24e82cbc4643b2cbf2b08acc3a3
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-10-24 13:29:49 +0800

    Add favicons

--- src/utilities/favicon.js
+++ src/utilities/favicon.js
@@ -1,18 +1,29 @@
 import { useEffect, useState } from 'react';
 
+const TWELVE_SECONDS = 1000 * 12;
+const favicons = [
+  './public/shirt.svg',
+  './public/dress.svg',
+  './public/beanie.svg',
+  './public/belt.svg',
+  './public/high-heel.svg',
+  './public/pants.svg',
+  './public/sneaker.svg',
+  './public/sock.svg',
+];
+
 export default function useFavicon() {
-  const [favicon, setFavicon] = useState('./public/shirt.svg');
-  const THIRTY_SECONDS = 1000 * 30;
+  const [favicon, setFavicon] = useState(0);
 
   useEffect(() => {
     const id = setInterval(() => {
-      if (favicon === './public/shirt.svg') setFavicon('./public/dress.svg');
-      else setFavicon('./public/shirt.svg');
-    }, THIRTY_SECONDS);
+      if (favicon < favicons.length - 1) setFavicon((f) => f + 1);
+      else setFavicon(0);
+    }, TWELVE_SECONDS);
 
     return () => clearInterval(id);
-  }, [favicon, THIRTY_SECONDS]);
+  }, [favicon]);
 
   const faviconEl = document.querySelector("link[rel~='icon']");
-  faviconEl.href = favicon;
+  faviconEl.href = favicons[favicon];
 }
