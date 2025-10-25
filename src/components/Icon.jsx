66c2cd144596a29f59d9923e07b616871886c747
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-10-25 16:36:00 +0800

    Add qty icon next to cart icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -6,16 +6,16 @@ import ItemAddedIcon from './icons/general/item-added.svg';
 
 import CartEmpty from './icons/cart/cart-empty.svg';
 import CartFull from './icons/cart/cart-full.svg';
-import Cart1Item from './icons/cart/cart-1.svg';
-import Cart2Item from './icons/cart/cart-2.svg';
-import Cart3Item from './icons/cart/cart-3.svg';
-import Cart4Item from './icons/cart/cart-4.svg';
-import Cart5Item from './icons/cart/cart-5.svg';
-import Cart6Item from './icons/cart/cart-6.svg';
-import Cart7Item from './icons/cart/cart-7.svg';
-import Cart8Item from './icons/cart/cart-8.svg';
-import Cart9Item from './icons/cart/cart-9.svg';
-import Cart9plusItem from './icons/cart/cart-9plus.svg';
+import Number1 from './icons/cart/cart-1.svg';
+import Number2 from './icons/cart/cart-2.svg';
+import Number3 from './icons/cart/cart-3.svg';
+import Number4 from './icons/cart/cart-4.svg';
+import Number5 from './icons/cart/cart-5.svg';
+import Number6 from './icons/cart/cart-6.svg';
+import Number7 from './icons/cart/cart-7.svg';
+import Number8 from './icons/cart/cart-8.svg';
+import Number9 from './icons/cart/cart-9.svg';
+import Number9plus from './icons/cart/cart-9plus.svg';
 
 const icons = {
   home: HomeIcon,
@@ -23,20 +23,26 @@ const icons = {
   itemAdded: ItemAddedIcon,
   cartEmpty: CartEmpty,
   cartFull: CartFull,
-  cart1: Cart1Item,
-  cart2: Cart2Item,
-  cart3: Cart3Item,
-  cart4: Cart4Item,
-  cart5: Cart5Item,
-  cart6: Cart6Item,
-  cart7: Cart7Item,
-  cart8: Cart8Item,
-  cart9: Cart9Item,
-  cart9plus: Cart9plusItem,
+  number1: Number1,
+  number2: Number2,
+  number3: Number3,
+  number4: Number4,
+  number5: Number5,
+  number6: Number6,
+  number7: Number7,
+  number8: Number8,
+  number9: Number9,
+  number9plus: Number9plus,
 };
 
 function Icon({ icon, alt }) {
-  return <img src={icons[icon]} alt={alt} className={styles.smallIcon} />;
+  return (
+    <img
+      src={icons[icon]}
+      alt={alt}
+      className={icon[0] === 'n' ? styles.miniIcon : styles.smallIcon}
+    />
+  );
 }
 
 export default Icon;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useStore } from '../contexts/storeReducer';
+
 import Icon from './Icon';
 import styles from './NavBar.module.css';
 
 function NavBar() {
+  const { cart } = useStore();
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.navList}>
         <li>
           <Link to="/">
             <Icon icon="home" alt="Home icon" />
-            Home
           </Link>
         </li>
         <li>
           <Link to="/shopping">
             <Icon icon="shopping" alt="Shopping icon" />
-            Shop
           </Link>
         </li>
         <li>
           <Link to="/cart">
             <Icon icon="cartEmpty" alt="Cart icon" />
-            Cart
+            {cart.length === 0 ? null : (
+              <Icon
+                icon={`number${cart.length < 10 ? cart.length : '9plus'}`}
+                alt="Cart icon"
+              />
+            )}
           </Link>
         </li>
       </ul>
