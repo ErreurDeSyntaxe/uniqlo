09adff880404b8ef6cdfe2356aab647bb74fc8b1
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-10-25 10:02:21 +0800

    Move data fetching to StoreContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router-dom';
-import { useEffect } from 'react';
 
 import './global.css';
-import { useStore } from './contexts/storeReducer';
-import useFavicon from './utilities/favicon';
-import fetchData from './utilities/fetchData';
-
 import Layout from './components/Layout';
 import Shopping from './pages/Shopping';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 
 function App() {
-  const { dispatch } = useStore();
-
-  useFavicon();
-
-  useEffect(() => {
-    async function fetching() {
-      dispatch({ type: 'loading', payload: null });
-      const data = await fetchData();
-      console.log(data);
-      dispatch({ type: 'ready', payload: data });
-    }
-    fetching();
-  }, [dispatch]);
-
   const router = createBrowserRouter([
     {
       path: '/',
--- src/contexts/StoreContext.jsx
+++ src/contexts/StoreContext.jsx
@@ -1,11 +1,22 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import { reducer, initialState } from './storeReducer';
+import fetchData from '../utilities/fetchData';
 
 const StoreContext = createContext();
 
 function StoreProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { products, cart, status } = state;
+
+  useEffect(() => {
+    async function fetching() {
+      dispatch({ type: 'loading', payload: null });
+      const data = await fetchData();
+      console.log(data);
+      dispatch({ type: 'ready', payload: data });
+    }
+    fetching();
+  }, [dispatch]);
 
   return (
     <StoreContext.Provider value={{ dispatch, products, cart, status }}>
