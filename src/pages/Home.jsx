c7d7dd9983187a91a94e8683e7f4aaf80aaf213d
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-10-25 10:21:47 +0800

    Redesign home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
+import { useStore } from '../contexts/storeReducer';
+
+import ProductCard from '../components/ProductCard';
 
 function Home() {
-  return <div className={styles.home}>Home</div>;
+  const { products } = useStore();
+  const lastBrowsed = products.slice(0, 4);
+
+  return (
+    <section className={styles.home}>
+      <div className={styles.hero}></div> {/* background hero */}
+      <h1 className={styles.heading}>Welcome back, Username!</h1>
+      <p className={styles.subheading}>
+        Revisit your favorite styles or explore new arrivals.
+      </p>
+      <div className={styles.lastBrowsed}>
+        <p className={styles.lastBrowsedTitle}>Last time, you browsed:</p>
+        <div className={styles.browsedContainer}>
+          {lastBrowsed.map((product, index) => (
+            <ProductCard key={index} product={product} />
+          ))}
+        </div>
+      </div>
+      <Link to="/shopping" className={styles.homeBtn}>
+        Shop Now
+      </Link>
+    </section>
+  );
 }
 
 export default Home;
