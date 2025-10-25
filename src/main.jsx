508a4a1c409de98b04070569f11d3d0470e614b0
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2025-10-25 10:02:47 +0800

    Move favicon logic to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { StoreProvider } from './contexts/StoreContext.jsx';
 
+import changeFavicon from './utilities/favicon.js';
+
+changeFavicon();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <StoreProvider>
